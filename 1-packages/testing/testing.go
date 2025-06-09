893f20ab50e5c4fbc3789d45be8ec0188f924cc3
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-09 19:05:41 -0300

    feat: add initial go.mod and go.sum files, and implement testing package with Write and write2 functions

---
+++ 1-packages/testing/testing.go
@@ -0,0 +1,8 @@
+package testing
+
+import "fmt"
+
+func Write() {
+	fmt.Println("Writing from testing")
+	write2()
+}
---
+++ 1-packages/testing/testing2.go
@@ -0,0 +1,7 @@
+package testing
+
+import "fmt"
+
+func write2() {
+	fmt.Println("Writing from testing2")
+}
