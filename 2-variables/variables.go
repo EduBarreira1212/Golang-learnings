b006bd683b3312f4bd5097ca0044c0269bc12007
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-10 18:38:03 -0300

    feat: implement initial variables and print functionality in main

---
+++ 2-variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Eduardo"
+	age := 21
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+	var (
+		var1 int = 1
+		var2 int = 2
+	)
+
+	fmt.Println(var1, var2)
+}
