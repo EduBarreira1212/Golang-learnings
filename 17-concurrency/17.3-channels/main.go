41820adf26d7b7d85f97cb6f6fc12a1d94d66ada
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-15 19:31:11 -0300

    feat: add initial implementation of main function and write function for concurrent text writing in Go

---
+++ 17-concurrency/17.3-channels/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	channel := make(chan string)
+
+	go write("Hello world", channel)
+
+	for message := range channel {
+		fmt.Println(message)
+	}
+
+	fmt.Println("END!")
+}
+
+func write(text string, channel chan string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(text)
+		time.Sleep(time.Second)
+	}
+
+	close(channel)
+}
