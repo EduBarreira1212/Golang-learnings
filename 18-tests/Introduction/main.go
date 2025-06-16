3c4becb586aa73bbb24ce4476a560a8c740fbd1a
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-16 17:29:50 -0300

    feat: add main function to demonstrate address type verification

---
+++ 18-tests/Introduction/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	address "tests-introduction/Address"
+)
+
+func main() {
+	addressType := address.VerifyAddressType("Street 123")
+	addressType2 := address.VerifyAddressType("street 1")
+	addressType3 := address.VerifyAddressType("Avenue 123")
+	addressType4 := address.VerifyAddressType("avenue 00000")
+
+	fmt.Println(addressType)
+	fmt.Println(addressType2)
+	fmt.Println(addressType3)
+	fmt.Println(addressType4)
+}
