29f9926b380928502b5c6e1b914c6916ff0daa5b
Eduardo Barreira <edubarreira1212@gmail.com>
2025-06-18 22:26:51 -0300

    feat: implement JSON marshalling for User struct

---
+++ 19-JSON/19.1-marshal/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func main() {
+	user1 := User{"Edu", "edu@gmail.com", "12345"}
+
+	user1JSON, err := json.Marshal(user1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(bytes.NewBuffer(user1JSON))
+}
